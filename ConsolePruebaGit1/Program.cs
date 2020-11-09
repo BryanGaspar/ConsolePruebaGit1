e7b918576c22b17b9d4916c91b69cd35b44d659c
BRYAN GASPAR <bryan.gaspar@pucese.edu.ec>
2020-11-09 09:31:44 -0500

    Revert "Funcion suma de dos números"
    This reverts commit 12b51b92b0413f0873454fa2085677cc17b07c74.

--- ConsolePruebaGit1/Program.cs
+++ ConsolePruebaGit1/Program.cs
@@ -11,18 +11,6 @@ namespace ConsolePruebaGit1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World con c#");
-
-            Console.WriteLine("Probando la funcion");
-            int suma = funcionSuma(10, 20);
-            Console.WriteLine("Resultado funcion = {0}", suma);
-            Console.ReadKey();
-        }
-
-        //Funcion suma
-        public static int funcionSuma(int a, int b)
-        {
-            return (a * 10 + b * 100);
-        
         }
     }
 }
