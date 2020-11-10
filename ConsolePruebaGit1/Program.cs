7cb37822559e2dfc22ad6220e3cdaf8f2ff41448
BRYAN GASPAR <bryan.gaspar@pucese.edu.ec>
2020-11-10 11:49:19 -0500

    funcion resta

--- ConsolePruebaGit1/Program.cs
+++ ConsolePruebaGit1/Program.cs
@@ -13,11 +13,16 @@ namespace ConsolePruebaGit1
             Console.WriteLine("Hello World con c#");
             Console.WriteLine("Probando la funcion");
             Console.WriteLine("La suma de 10 + 20 es {0}", funcionSuma(10, 20));
+            Console.WriteLine("La resta de 10 - 20 es {0}", resta(10, 20));
             Console.ReadKey();
         }
         public static int funcionSuma(int a, int b)
         {
             return (a + b);
+        }
+        public static int resta(int a, int b)
+        {
+            return (a - b);
         }
     }
 }
